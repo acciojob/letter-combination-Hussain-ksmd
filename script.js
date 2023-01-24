ee08e1feb7a40d3c8a28d5ad7d788db6e13aa932
Mohammad Hussain <95843547+Hussain-ksmd@users.noreply.github.com>
2023-01-24 21:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+    if (!digits) {
+        return [];
+    }
+    const map = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+        '0': '0',
+        '1': '1'
+    };
+    let result = [''];
+    for (let digit of digits) {
+        let temp = [];
+        for (let str of result) {
+            for (let ch of map[digit]) {
+                temp.push(str + ch);
+            }
+        }
+        result = temp;
+    }
+    return result.sort();
 }
 
 module.exports = letterCombinations;
